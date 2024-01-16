d98d02c8608a6dd24b2b2c738eab466b26ae7972
Abhinav-3009 <abhi30092000.ag@gmail.com>
2024-01-16 17:40:16 +0530

    [mainAG]: modified the single product page to show the basic information like title, price, description, etc.

--- src/context/productcontext.js
+++ src/context/productcontext.js
@@ -7,38 +7,52 @@ const AppContext = createContext();
 const API = "https://api.pujakaitem.com/api/products";
 
 const initialState = {
-    isLoading: false,
-    isError: false,
-    products: [],
-    featureProducts: [],
-  };
-  
-
-const AppProvider = ({children})=>{
-    const [state, dispatch] = useReducer(reducer, initialState);
-
-    const getProducts = async (url) => {
-        dispatch({ type: "SET_LOADING" });
-        try {
-          const res = await axios.get(url);
-          const products = await res.data;
-          dispatch({ type: "SET_API_DATA", payload: products });
-        } catch (error) {
-          dispatch({ type: "API_ERROR" });
-        }
-      };
-
-    useEffect(()=>{
-        getProducts(API);
-    },[]);
-
-    return (
-        <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
-      );
+  isLoading: false,
+  isError: false,
+  products: [],
+  featureProducts: [],
+  isSingleLoading: false,
+  singleProduct: {},
 };
 
-const useProductContext = ()=>{
-    return useContext(AppContext);
-}
+const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-export {AppProvider, AppContext, useProductContext};
+  const getProducts = async (url) => {
+    dispatch({ type: "SET_LOADING" });
+    try {
+      const res = await axios.get(url);
+      const products = await res.data;
+      dispatch({ type: "SET_API_DATA", payload: products });
+    } catch (error) {
+      dispatch({ type: "API_ERROR" });
+    }
+  };
+
+  const getSingleProduct = async (url) => {
+    dispatch({ type: "SET_SINGLE_LOADING" });
+    try {
+      const res = await axios.get(url);
+      const singleProduct = await res.data;
+      dispatch({ type: "SET_SINGLE_PRODUCT", payload: singleProduct });
+    } catch (error) {
+      dispatch({ type: "SET_SINGLE_ERROR" });
+    }
+  };
+
+  useEffect(() => {
+    getProducts(API);
+  }, []);
+
+  return (
+    <AppContext.Provider value={{ ...state, getSingleProduct }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+const useProductContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppProvider, AppContext, useProductContext };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import HeroSection from "../components/HeroSection";
 import Services from "../components/Services";
 import Trusted from "../components/Trusted";
-import FeatureProduct from "../components/FeaturedProducts";
+import FeatureProduct from "../components/FeatureProduct";
 
 const Home = () => {
   const data = {
